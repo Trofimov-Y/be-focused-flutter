8ce61fb0fc031a4d4ace90b54923b59b532f33cb
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-01-06 10:09:20 +0200

    feat: create project from Very Good CLI

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.be_focused
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:be_focused/app/app.dart';
+import 'package:be_focused/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
