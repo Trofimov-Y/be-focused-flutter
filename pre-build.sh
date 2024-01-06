ebd4cb7fd6e4e18cc84b4dd3dc31ba39797bbab5
Yuriy Trofimov <74924170+Trofimov-Y@users.noreply.github.com>
2024-01-06 11:11:46 +0200

    build: Change translations script
    * build: update localization script
    
    * fix: change translations
    
    * ci: add empty test folder
    
    * test: add main test

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 dart run po_editor_translations --api_token=4c740314004bc10661b6ef8db3347beb --project_id=673153 --files_path=lib/l10n/arb
+flutter gen-l10n --arb-dir="lib/l10n/arb"
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:be_focused/app/app.dart';
+import 'package:be_focused/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
