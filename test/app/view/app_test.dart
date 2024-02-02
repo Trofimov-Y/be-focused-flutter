78ab59724e7c964c594e00509bb295d711dc5a94
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-02-02 14:48:52 +0200

    style: fix style erros

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,9 +1,7 @@
-import 'package:be_focused/app/app.dart';
-import 'package:be_focused/features/home/presentation/pages/home_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // TODO: (Yuriy-Trofimov) Add more tests
+  // TODO(Yuriy-Trofimov): Add more tests
   test('Always passed test', () {
     const value = 1;
     expect(value, 1);
