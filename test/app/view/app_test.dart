e00d857ab66574350883e2484fa1ca4326c16cd5
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-02-02 14:23:09 +0200

    test: change main test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:be_focused/app/app.dart';
-import 'package:be_focused/counter/counter.dart';
+import 'package:be_focused/features/home/presentation/pages/home_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders Home Page', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
