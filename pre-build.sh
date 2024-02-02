7b8a9ec936324122ce579bcadc14ce5bd7f83102
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-02-02 14:33:48 +0200

    ci: update config

--- pre-build.sh
+++ pre-build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 dart run po_editor_translations --api_token=4c740314004bc10661b6ef8db3347beb --project_id=673153 --files_path=lib/l10n/arb
 flutter gen-l10n --arb-dir="lib/l10n/arb"
+flutter pub run build_runner build --delete-conflicting-outputs
