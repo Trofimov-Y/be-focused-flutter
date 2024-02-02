75be6c64d96cf5f127ab386a1c687364e78cb2ba
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-02-02 14:15:26 +0200

    feat: add app theme

---
+++ lib/app/theme/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const colorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Colors.white,
+  onPrimary: Colors.black87,
+  secondary: Colors.black,
+  onSecondary: Color(0xFF1d1d1d),
+  error: Color(0xffffb4ab),
+  onError: Color.fromRGBO(233, 64, 37, 1),
+  background: Colors.black,
+  onBackground: Color(0xE6000000),
+  surface: Color(0xff191113),
+  onSurface: Colors.white70,
+);
+
+class CardColors {
+  static const red = Color.fromRGBO(233, 64, 37, 1);
+  static const yellow = Color.fromRGBO(244, 184, 64, 1);
+  static const strawberry = Color.fromRGBO(237, 106, 101, 1);
+  static const emerald = Color(0xFF00bf8f);
+  static const pink = Color.fromRGBO(246, 201, 232, 1);
+}
