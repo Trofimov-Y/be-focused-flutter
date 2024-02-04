b9cafd10c6fec1af5e8dea13db35a94148f5517c
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-02-04 12:43:25 +0200

    fix: pre-build.sh script

--- pre-build.sh
+++ pre-build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 dart run flutter_loco_translations --locales="en" --api_key="bImSTiTrbG2UYvuDx55rwX-W0h7W1nu7" --path="lib/l10n/arb"
-#dart run po_editor_translations --api_token=4c740314004bc10661b6ef8db3347beb --project_id=673153 --files_path=lib/l10n/arb
 flutter gen-l10n --arb-dir="lib/l10n/arb"
-#flutter pub run build_runner build --delete-conflicting-outputs
+flutter pub run build_runner build --delete-conflicting-outputs
