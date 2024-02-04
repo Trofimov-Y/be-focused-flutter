407c4296bdd6da9010d745add1d6a1571a6a7454
Yuriy Trofimov <yuriy.trofimov.ua@gmail.com>
2024-02-04 12:32:41 +0200

    fix: imports

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,7 @@
+import 'package:be_focused/app/observers/auto_router_observer.dart';
 import 'package:be_focused/app/routing/router.dart';
 import 'package:be_focused/app/theme/theme_data.dart';
 import 'package:be_focused/l10n/l10n.dart';
-import 'package:be_focused/observers/auto_router_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
